5cfd9d8b151be04f91b97e9af0818794872dfc42
Brandon Hernandez <brandon@balam.rocks>
2021-06-14 17:44:25 -0500

    :hammer: UPDATE: Se agrega el ejercicio de entrada de datos

---
+++ EntradaDeDatos.py
@@ -0,0 +1,22 @@
+tema_curso = "== Entrada de datos =="
+print(tema_curso)
+
+#Entrada de datos String
+nombre = input("Ingresa un nombre: ")
+
+#Entrada de datos int
+numeroEntero = int(input("Introduce un numero entero: "))
+
+#Entrada de numero flotante
+numeroFlotante = float(input("Introduce un numero flotante: "))
+
+#Salida de lo introducido
+# print("Nombre: ", nombre)
+print("Numero entero: ", numeroEntero)
+print("Numero flotante: ", numeroFlotante)
+
+#Ejercicio de suma de dos numeros ingresados
+numeroUno = int(input("Ingresa el primer valor: "))
+numeroDos = int(input("Ingresa el segundo valor: "))
+suma = numeroUno + numeroDos
+print("El resultado es: ", suma)
