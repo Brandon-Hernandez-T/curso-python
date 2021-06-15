37140cd077ee6adbd1d72ba34bab9a750c3f3535
Brandon Hernandez <brandon@balam.rocks>
2021-06-15 17:54:38 -0500

    :hammer: UPDATE: Se realiza la creacionde models y referentes de BBDD

--- Django/TiendaOnline/gestionPedidos/models.py
+++ Django/TiendaOnline/gestionPedidos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Una clase por cada Tabla
+#Tabla: Clientes
+class Clientes(models.Model):
+    nombre=models.CharField(max_length=30)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField()
+    telefono=models.CharField(max_length=10)
+
+#Tabla: Articulos
+class Articulos(models.Model):
+    nombre=models.CharField(max_length=30)
+    seccion=models.CharField(max_length=20)
+    precio=models.IntegerField()
+
+#Tabla: Pedidos
+class Pedidos(models.Model):
+    numero=models.IntegerField()
+    fecha=models.IntegerField()
+    entregado=models.BooleanField()
