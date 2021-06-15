b66052989174c081aac8bd5e3e25409bf79c28c3
Brandon Hernandez <brandon@balam.rocks>
2021-06-15 01:26:29 -0500

    :hammer: UPDATE: Se realiza ejercicio para ingresar un string desde la url

--- Django/Projecto2/Projecto2/urls.py
+++ Django/Projecto2/Projecto2/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from Projecto2.views import calculaEdad, dameFecha, despedida, saludo
+from Projecto2.views import calculaEdad, dameFecha, despedida, mensajeUsuario, saludo
 from django.contrib import admin
 from django.urls import path
 
@@ -22,5 +22,6 @@ urlpatterns = [
     path('saludo/', saludo),
     path('despedida/', despedida),
     path('fecha/', dameFecha),
-    path('edades/<int:edad>/<int:anio>', calculaEdad)
+    path('edades/<int:edad>/<int:anio>', calculaEdad),
+    path('mensaje/<str:mensaje>', mensajeUsuario)
 ]
--- Django/Projecto2/Projecto2/views.py
+++ Django/Projecto2/Projecto2/views.py
@@ -2,24 +2,39 @@ from django.http import HttpResponse
 import datetime
 
 # A cada funcion se le denomina vista, asi que esta es nuetsra primera vista
-def saludo(request) :
+
+
+def saludo(request):
     mensaje = "Hola esta es el primer response"
     return HttpResponse(mensaje)
 
 # Esta seria nuestra segunda vista.
+
+
 def despedida(request):
     return HttpResponse("Hasta luego bros!")
 
-#Esta es la tercer vista: 
+# Esta es la tercer vista:
+
+
 def dameFecha(request):
     fecha_actual = datetime.datetime.now()
     mensaje = "fecha y hora actuales: " + str(fecha_actual)
 
     return HttpResponse(mensaje)
 
+
 def calculaEdad(request, edad, anio):
     periodo = anio-2021
     edadFutura = edad + periodo
-    mensaje = "En el año %s tendras %s años" %(anio, edadFutura)
-    
+    mensaje = "En el año %s tendras %s años" % (anio, edadFutura)
+
     return HttpResponse(mensaje)
+
+
+def mensajeUsuario(request, mensaje):
+        if mensaje == 'brandon':
+            output = "Ingreso"
+        else:
+            output = "Incorrecto"    
+        return HttpResponse(output)
